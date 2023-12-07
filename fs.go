9fe7b83bdb1ebeac8d95490f7d85ddf629e8eccb
Terry Hardie <65614981+sfc-gh-thardie@users.noreply.github.com>
2023-12-07 13:17:27 -0800

    Update fs.go
    Co-authored-by: Paulo Gomes <paulo.gomes.uk@gmail.com>

--- fs.go
+++ fs.go
@@ -164,7 +164,8 @@ type File interface {
 	// Name returns the name of the file as presented to Open.
 	Name() string
 	io.Writer
-	io.WriterAt
+	// TODO: Add io.WriterAt for v6  
+	// io.WriterAt
 	io.Reader
 	io.ReaderAt
 	io.Seeker
