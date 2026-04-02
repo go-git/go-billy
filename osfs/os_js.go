daa74763deb658f1053d661cf67da725f60772fd
Christian Stewart <christian@aperture.us>
2026-04-02 10:45:46 -0700

    fix: bring os_js.go up to date with os.go
    Recent changes to os_js.go added WithBoundOS and ChrootOSFS.
    
    These changes were not propagated to os_js.go, which results in
    the following build failures in go-git:
    
    % GOOS=js GOARCH=wasm gb ./...
    github.com/go-git/go-git/v6
    ./remote.go:214:32: undefined: osfs.WithBoundOS
    ./repository.go:348:29: undefined: osfs.WithBoundOS
    ./repository.go:353:28: undefined: osfs.WithBoundOS
    ./repository.go:460:28: undefined: osfs.WithBoundOS
    ./repository.go:468:44: undefined: osfs.WithBoundOS
    ./repository.go:528:32: undefined: osfs.WithBoundOS
    ./repository.go:531:46: undefined: osfs.WithBoundOS
    ./repository.go:551:36: undefined: osfs.WithBoundOS
    ./repository.go:553:62: undefined: osfs.WithBoundOS
    
    This commit brings os_js.go up to date with os.go to fix this.
    
    All tests pass with GOOS=js GOARCH=wasm now.
    
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- osfs/os_js.go
+++ osfs/os_js.go
@@ -3,6 +3,9 @@
 package osfs
 
 import (
+	"path/filepath"
+	"strings"
+
 	"github.com/go-git/go-billy/v6"
 	"github.com/go-git/go-billy/v6/helper/chroot"
 	"github.com/go-git/go-billy/v6/memfs"
@@ -16,8 +19,105 @@ var globalMemFs = memfs.New()
 var Default = memfs.New()
 
 // New returns a new OS filesystem.
-func New(baseDir string, _ ...Option) billy.Filesystem {
+// By default paths are deduplicated, but still enforced
+// under baseDir. For more info refer to WithDeduplicatePath.
+func New(baseDir string, opts ...Option) billy.Filesystem {
+	o := &options{
+		deduplicatePath: true,
+	}
+	for _, opt := range opts {
+		opt(o)
+	}
+
+	if o.Type == BoundOSFS {
+		return newBoundOS(baseDir, o.deduplicatePath)
+	}
+
+	return newChrootOS(baseDir)
+}
+
+func newChrootOS(baseDir string) billy.Filesystem {
 	return chroot.New(Default, Default.Join("/", baseDir))
 }
 
-type options struct{}
+// BoundOS is a fs implementation based on the js/wasm in-memory filesystem
+// which is bound to a base dir.
+type BoundOS struct {
+	billy.Filesystem
+	baseDir         string
+	deduplicatePath bool
+}
+
+func newBoundOS(d string, deduplicatePath bool) billy.Filesystem {
+	baseDir := globalMemFs.Join("/", d)
+	return &BoundOS{
+		Filesystem:      chroot.New(globalMemFs, baseDir),
+		baseDir:         baseDir,
+		deduplicatePath: deduplicatePath,
+	}
+}
+
+// Chroot returns a new BoundOS filesystem, with the base dir set to the
+// result of joining the provided path with the underlying base dir.
+func (fs *BoundOS) Chroot(path string) (billy.Filesystem, error) {
+	joined, err := fs.abs(path)
+	if err != nil {
+		return nil, err
+	}
+	return New(joined, WithBoundOS(), WithDeduplicatePath(fs.deduplicatePath)), nil
+}
+
+// Root returns the current base dir of the billy.Filesystem.
+func (fs *BoundOS) Root() string {
+	return fs.baseDir
+}
+
+func (fs *BoundOS) abs(filename string) (string, error) {
+	if filename == "." || filename == "" {
+		return fs.baseDir, nil
+	}
+
+	filename = filepath.ToSlash(filename)
+	filename = filepath.Clean(filename)
+	if strings.HasPrefix(filename, "..") {
+		return "", billy.ErrCrossedBoundary
+	}
+
+	filename = strings.TrimPrefix(filename, string(filepath.Separator))
+	return filepath.Clean(filepath.Join(fs.baseDir, filepath.FromSlash(filename))), nil
+}
+
+// WithBoundOS returns the option of using a Bound filesystem OS.
+func WithBoundOS() Option {
+	return func(o *options) {
+		o.Type = BoundOSFS
+	}
+}
+
+// WithChrootOS returns the option of using a Chroot filesystem OS.
+func WithChrootOS() Option {
+	return func(o *options) {
+		o.Type = ChrootOSFS
+	}
+}
+
+// WithDeduplicatePath toggles the deduplication of the base dir in the path.
+// This option is accepted for API parity with non-js builds and has no effect
+// on the js/wasm in-memory implementation.
+func WithDeduplicatePath(enabled bool) Option {
+	return func(o *options) {
+		o.deduplicatePath = enabled
+	}
+}
+
+type options struct {
+	Type
+	deduplicatePath bool
+}
+
+type Type int
+
+const (
+	ChrootOSFS Type = iota
+	BoundOSFS
+)
